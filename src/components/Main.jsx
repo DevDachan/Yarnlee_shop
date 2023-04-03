a57a79f3f0f75673e3ab0bd410ca354ff5a28106
DevDachan <dachan9524@gmail.com>
2023-04-03 19:33:03 +0900

    feat: Add react product rendering in main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
+import axios from "axios";
 import ItemList from "./list/ItemList";
 // 나중에 현재 전체 목록 itemList로 변경하기.
 
+const Wrapper = styled.div`
+    padding: 0 2.5em;
+    margin: 0 auto;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
 function Main(props) {
+  const [productList, setProductList] = useState();
   const navigate = useNavigate();
-  const Wrapper = styled.div`
-      padding: 0 2.5em;
-      margin: 0 auto;
-      width: calc(100% - 32px);
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-  `;
+
+  var product_content = '';
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: 'http://localhost:8090/shop-backend/product/productList'
+    })
+    .then(function (response){
+      //handle success
+      setProductList(response.data.productContent);
+    })
+    .catch(function(error){
+      //handle error
+      console.log(error);
+    })
+    .then(function(){
+      // always executed
+    });
+  },[]);
+
+  function makeProductContent(){
+    var arr = [];
+
+    for(var i = 0; i < productList.length; i++){
+      arr.push(
+        <article className={"style"+(i+1)}>
+          <span className="image">
+            <img src={"images/pic0"+(i+1)+".jpg"} alt="" />
+          </span>
+          <a href="content">
+            <h2>{productList[i].name}</h2>
+            <div className="content">
+              <p>{productList[i].content}.</p>
+            </div>
+          </a>
+        </article>);
+    }
+    return arr;
+
+  }
+
+
   return (
       <Wrapper>
           <div id="main">
@@ -24,133 +69,15 @@ function Main(props) {
                 <h1>Only Handmade</h1>
               </header>
               <section className="tiles">
+                {
+                  productList == undefined ? "": makeProductContent()
+                }
                 <article className="style1">
                   <span className="image">
                     <img src="images/pic01.jpg" alt="" />
                   </span>
                   <a href="content">
                     <h2>Magna</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style2">
-                  <span className="image">
-                    <img src="images/pic02.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Lorem</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style3">
-                  <span className="image">
-                    <img src="images/pic03.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Feugiat</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style4">
-                  <span className="image">
-                    <img src="images/pic04.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Tempus</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style5">
-                  <span className="image">
-                    <img src="images/pic05.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Aliquam</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style6">
-                  <span className="image">
-                    <img src="images/pic06.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Veroeros</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style2">
-                  <span className="image">
-                    <img src="images/pic07.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Ipsum</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style3">
-                  <span className="image">
-                    <img src="images/pic08.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Dolor</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style1">
-                  <span className="image">
-                    <img src="images/pic09.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Nullam</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style5">
-                  <span className="image">
-                    <img src="images/pic10.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Ultricies</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style6">
-                  <span className="image">
-                    <img src="images/pic11.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Dictum</h2>
-                    <div className="content">
-                      <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
-                    </div>
-                  </a>
-                </article>
-                <article className="style4">
-                  <span className="image">
-                    <img src="images/pic12.jpg" alt="" />
-                  </span>
-                  <a href="content">
-                    <h2>Pretium</h2>
                     <div className="content">
                       <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
                     </div>
