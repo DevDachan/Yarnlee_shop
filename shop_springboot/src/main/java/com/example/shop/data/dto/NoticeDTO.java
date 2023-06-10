d74ddd5016852ded6a38f32d8fe980968931ee21
DevDachan <dachan9524@gmail.com>
2023-06-10 12:04:44 +0900

    feat: Add notice Entity

---
+++ shop_springboot/src/main/java/com/example/shop/data/dto/NoticeDTO.java
@@ -0,0 +1,37 @@
+package com.example.shop.data.dto;
+
+import com.example.shop.data.entity.NoticeEntity;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class NoticeDTO {
+  @NotNull
+  private int id;
+
+  @NotNull
+  private int title;
+
+  private String content;
+
+  @NotNull
+  private int hits;
+
+  public NoticeEntity toEntity(){
+    return NoticeEntity.builder()
+        .id(id)
+        .title(title)
+        .content(content)
+        .hits(hits)
+        .build();
+  }
+
+}
