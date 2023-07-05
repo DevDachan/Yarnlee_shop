41241f0efa4bbcfaa43880f51677dc8387887b53
DevDachan <dachan9524@gmail.com>
2023-07-05 23:21:20 +0900

    etc: change config password

--- shop_springboot/src/main/java/com/example/shop/configuration/CorsConfig.java
+++ shop_springboot/src/main/java/com/example/shop/configuration/CorsConfig.java
@@ -1,20 +1,20 @@
 package com.example.shop.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+  @Value("${cors-path}")
+  private String path;
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000","http://localhost:80","http://104.198.11.59","http://localhost" ,"http://104.198.11.59:80", "http://104.198.11.59:3000")
+        .allowedOrigins(path)
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .allowCredentials(true);
   }
 }
-//@CrossOrigin(origins = "http://localhost:3000")
-//Origins("http://104.198.11.59:3000")
