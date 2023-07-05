e4e6bc258ee8b020c1cf32c0fe18db245d299ce8
DevDachan <dachan9524@gmail.com>
2023-07-05 23:21:20 +0900

    etc: change config password

--- shop_springboot/src/main/java/com/example/shop/configuration/JasyptConfig.java
+++ shop_springboot/src/main/java/com/example/shop/configuration/JasyptConfig.java
@@ -4,6 +4,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,15 +14,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfig {
+  @Value("${Jasypt-password}")
+  private String password;
+  @Value("${Jasypt-algorithm}")
+  private String algorithm;
 
   @Bean(name = "jasyptStringEncryptor")
   public StringEncryptor stringEncryptor() {
-    String password = "Demo";
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
     config.setPassword(password); // 암호화할 때 사용하는 키
-    config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+    config.setAlgorithm(algorithm); // 암호화 알고리즘
     config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
     config.setPoolSize("1"); // 인스턴스 pool
     config.setProviderName("SunJCE");
