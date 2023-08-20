063e36215d406e4f3b3be341fb58d1e3bdac7e27
DevDachan <dachan9524@gmail.com>
2023-08-20 23:03:45 +0900

    fix: orderHistory error fix

--- shop_springboot/src/main/java/com/example/shop/configuration/CorsConfig.java
+++ shop_springboot/src/main/java/com/example/shop/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://104.198.11.59","http://104.198.11.59:80",
-            "http://localhost:3000", "http://104.198.11.59:3000")
+            "http://localhost:3000","http://104.198.11.59:3000")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .allowCredentials(true);
